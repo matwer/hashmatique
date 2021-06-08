b69a7162afe31b436cd89ba4f2510c41629993f6
Matthew Werner <matwer@outlook.com>
2021-06-08 11:30:26 -0700

    updated the println statments

--- Hashmatique.java
+++ Hashmatique.java
@@ -17,8 +17,8 @@ public class Hashmatique {
         // loop throught the Hashmap
         Set<String> Tracks = trackList.keySet();
         for(String Track : Tracks) {
-            System.out.println(Track);
-            System.out.println(trackList.get(Track));   
+            System.out.println("\nTrack: " + Track);
+            System.out.println("Lyrics: " + trackList.get(Track));   
         }
     }
 }
